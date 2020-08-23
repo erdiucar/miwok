0db71bf6b8ef029da9688659fdb141f7850ebfd3
Erdi Uçar <ucarerdi@gmail.com>
2020-08-24 01:31:29 +0300

    Add a new Word and WordAdapter(extends from ArrayAdapter) class for ListView

---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,27 @@
+package com.example.android.miwok;
+
+public class Word {
+    private String defaultTranslation;
+    private String miwokTranslation;
+
+    public Word(String defaultTranslation, String miwokTranslation) {
+        this.defaultTranslation = defaultTranslation;
+        this.miwokTranslation = miwokTranslation;
+    }
+
+    public String getDefaultTranslation() {
+        return defaultTranslation;
+    }
+
+    public void setDefaultTranslation(String defaultTranslation) {
+        this.defaultTranslation = defaultTranslation;
+    }
+
+    public String getMiwokTranslation() {
+        return miwokTranslation;
+    }
+
+    public void setMiwokTranslation(String miwokTranslation) {
+        this.miwokTranslation = miwokTranslation;
+    }
+}
---
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,39 @@
+package com.example.android.miwok;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class WordAdapter extends ArrayAdapter<Word> {
+    public WordAdapter(@NonNull Context context, @NonNull ArrayList<Word> objects) {
+        super(context, 0, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+
+        Word currentWord = getItem(position);
+
+        TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        miwokTextView.setText(currentWord.getMiwokTranslation());
+
+        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+        defaultTextView.setText(currentWord.getDefaultTranslation());
+
+        return listItemView;
+    }
+}
