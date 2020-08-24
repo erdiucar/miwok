3adb7017a664a4d514a5537a0acbc81a46b90ea7
Erdi Uçar <ucarerdi@gmail.com>
2020-08-24 21:05:22 +0300

    Add background color to the WordAdapter and add image to the Word class

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -1,27 +1,38 @@
 package com.example.android.miwok;
 
+import android.graphics.Color;
+
 public class Word {
+    private static final int N0_IMAGE_PROVIDED = -1;
     private String defaultTranslation;
     private String miwokTranslation;
+    private int imageResourceId = N0_IMAGE_PROVIDED;
+    private Color color;
 
     public Word(String defaultTranslation, String miwokTranslation) {
         this.defaultTranslation = defaultTranslation;
         this.miwokTranslation = miwokTranslation;
     }
 
-    public String getDefaultTranslation() {
-        return defaultTranslation;
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId) {
+        this.defaultTranslation = defaultTranslation;
+        this.miwokTranslation = miwokTranslation;
+        this.imageResourceId = imageResourceId;
     }
 
-    public void setDefaultTranslation(String defaultTranslation) {
-        this.defaultTranslation = defaultTranslation;
+    public String getDefaultTranslation() {
+        return defaultTranslation;
     }
 
     public String getMiwokTranslation() {
         return miwokTranslation;
     }
 
-    public void setMiwokTranslation(String miwokTranslation) {
-        this.miwokTranslation = miwokTranslation;
+    public int getImageResourceId() {
+        return imageResourceId;
+    }
+
+    public boolean hasImage() {
+        return imageResourceId != N0_IMAGE_PROVIDED;
     }
 }
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -1,19 +1,27 @@
 package com.example.android.miwok;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 class WordAdapter extends ArrayAdapter<Word> {
-    public WordAdapter(@NonNull Context context, @NonNull ArrayList<Word> words) {
+    private int colorResourceId;
+
+    public WordAdapter(@NonNull Context context, @NonNull ArrayList<Word> words, @NonNull int colorResourceId) {
         super(context, 0, words);
+
+        this.colorResourceId = colorResourceId;
     }
 
     @NonNull
@@ -26,11 +34,23 @@ class WordAdapter extends ArrayAdapter<Word> {
 
         Word currentWord = getItem(position);
 
+        View textContainer = listItemView.findViewById(R.id.text_container);
+        textContainer.setBackgroundColor(ContextCompat.getColor(getContext(), colorResourceId));
+
         TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
         miwokTextView.setText(currentWord.getMiwokTranslation());
 
         TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
         defaultTextView.setText(currentWord.getDefaultTranslation());
+
+        ImageView image = (ImageView) listItemView.findViewById(R.id.image);
+        if (currentWord.hasImage()) {
+            image.setImageResource(currentWord.getImageResourceId());
+
+            image.setVisibility(View.VISIBLE);
+        } else {
+            image.setVisibility(View.GONE);
+        }
 
         return listItemView;
     }
